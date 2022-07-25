f8636c93e7c3efa2448a2cb1fa4bf1ca8711b601
Joseph Kurpierz <josephkurpierz@gmail.com>
2022-07-25 18:59:59 -0500

    post routes

--- models/index.js
+++ models/index.js
@@ -1,4 +1,12 @@
-const User = require('./Users');
-const Post = require('./Post');
+const User = require("./Users");
+const Post = require("./Post");
 
-module.exports = {User, Post};
+User.hasMany(Post, {
+  foreignKey: "user_id",
+});
+
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+module.exports = { User, Post };
