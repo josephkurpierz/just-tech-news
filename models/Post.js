757343b13bc20b338365c710f262f863181846a9
Joseph Kurpierz <josephkurpierz@gmail.com>
2022-07-29 10:27:44 -0500

    post model refactor

--- models/Post.js
+++ models/Post.js
@@ -2,40 +2,65 @@ const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 
 //create post model
-class Post extends Model {};
+class Post extends Model {
+  static upvote(body, models) {
+    return models.Vote.create({
+      user_id: body.user_id,
+      post_id: body.post_id,
+    }).then(() => {
+      return Post.findOne({
+        where: {
+          id: body.post_id,
+        },
+        attributes: [
+          "id",
+          "post_url",
+          "title",
+          "created_at",
+          [
+            sequelize.literal(
+              "(Select Count(*) FROM vote WHERE post.id = vote.post_id)"
+            ),
+            "vote_count",
+          ],
+        ],
+      });
+    });
+  }
+}
 
 Post.init(
   {
-    id:{
+    id: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      primaryKey:true,
-      autoIncrement: true
+      primaryKey: true,
+      autoIncrement: true,
     },
-    title:{
-      type: DataTypes.STRING,
-      allowNull:false
-    },
-    post_url:{
+    title: {
       type: DataTypes.STRING,
       allowNull: false,
-      validate:{
-        isURL:true
-      }
     },
-    user_id:{
+    post_url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isURL: true,
+      },
+    },
+    user_id: {
       type: DataTypes.INTEGER,
-      references:{
-        model:'user',
-        key:'id'
-      }
-    }
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
-    freezeTableName:true,
-    underscored:true,
-    modelName: 'post'
+    freezeTableName: true,
+    underscored: true,
+    modelName: "post",
   }
 );
 
