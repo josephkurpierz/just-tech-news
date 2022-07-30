920f747a4f8b8d149d8f0603ab40e8cdbb1169f5
Joseph Kurpierz <josephkurpierz@gmail.com>
2022-07-30 18:08:36 -0500

    homepage handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 
+const homeRoutes = require('./home_routes');
 const apiRoutes = require('./api');
 
 router.use('/api',apiRoutes);
-
+router.use('/',homeRoutes)
 router.use((req,res)=>{
   res.status(404).end();
 });
