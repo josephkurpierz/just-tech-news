c6756c2fd50ae820da5a53fec5845c404f06b4df
Joseph Kurpierz <josephkurpierz@gmail.com>
2022-07-31 11:15:04 -0500

    logout

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -61,32 +61,59 @@ router.post("/", (req, res) => {
     email: req.body.email,
     password: req.body.password
   })
-  .then(dbUserData => res.json(dbUserData))
+  .then(dbUserData => {
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+  
+      res.json(dbUserData);
+    });
+  })
   .catch(err=>{
     console.log(err);
     res.status(500).json(err);
   });
 });
 
-router.post("/login",(req,res)=>{
+router.post('/login', (req, res) => {
   User.findOne({
-    where:{
+    where: {
       email: req.body.email
     }
-  })
-  .then(dbUserData=>{
-    if(!dbUserData){
-      res.status(400).json({message:'No user with that email address'});
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!' });
       return;
     }
-  
+
     const validPassword = dbUserData.checkPassword(req.body.password);
-    if(!validPassword){
-      res.status(400).json({message:'Incorrect password!'});
+
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
       return;
     }
-    res.json({user:dbUserData, message: 'You are now logged in'});
+
+    req.session.save(() => {
+      // declare session variables
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
   });
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  }
+  else {
+    res.status(404).end();
+  }
 });
 
 //update api/user
