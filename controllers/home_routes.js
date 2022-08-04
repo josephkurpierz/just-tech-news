f1d782529f875a910d5ff8f66fca5c408bd15aba
Joseph Kurpierz <josephkurpierz@gmail.com>
2022-08-04 13:22:58 -0500

    single post

--- controllers/home_routes.js
+++ controllers/home_routes.js
@@ -30,7 +30,10 @@ router.get('/', (req, res) => {
     .then(dbPostData => {
       // pass a single post object into the homepage template
       const posts = dbPostData.map(post => post.get({ plain: true }));
-      res.render('homepage', {posts});
+      res.render('homepage', {
+        posts,
+        loggedIn: req.session.loggedIn
+      });
     })
     .catch(err => {
       console.log(err);
@@ -80,7 +83,10 @@ router.get('/post/:id', (req,res) => {
     }
     //serialize the data
     const post = dbPostData.get({plain:true});
-    res.render('single-post',{post});
+    res.render('single-post', {
+      post,
+      loggedIn: req.session.loggedIn
+    });
   })
   .catch(err => {
     console.log(err);
